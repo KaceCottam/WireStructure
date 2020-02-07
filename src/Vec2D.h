66beeef35fa83088b3189dd39e9f4b050dfd9644
Kace Cottam <kace.cottam@wsu.edu>
2020-02-07 15:30:40 -0800

    Begin remake, starting with Vec2D

--- src/Vec2D.h
+++ src/Vec2D.h
@@ -1,68 +1,57 @@
 #ifndef VEC2D_H
 #define VEC2D_H
-#include <cassert>
-#include <cmath>
-struct Vec2D
-{
-  Vec2D(const int x = 0, const int y = 0) : x{x} , y{y} { }
+#include <utility>
+#include <functional>
+template<class T>
+class Vec2D {
+  using base = std::pair<T, T>;
+  base pair;
 
-  template<class Container>
-  Vec2D(Container&& values) : Vec2D{values[0], values[1]} { }
+ public:
+  template<class... Args>
+  Vec2D(Args&&... args) : pair{std::forward<Args>(args)...} {}
 
-  int x, y;
+  T& x = pair.first;
+  T& y = pair.second;
 
-  [[nodiscard]] friend Vec2D operator+(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return Vec2D(a.x + b.x, a.y + b.y);
-  }
-  [[nodiscard]] friend Vec2D operator-(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return a + (-1 * b);
-  }
-  [[nodiscard]] friend Vec2D operator*(const Vec2D& a, const int scal) noexcept
-  {
-    return Vec2D(a.x * scal, a.y * scal);
-  }
-  [[nodiscard]] friend Vec2D operator*(const int scal, const Vec2D& a) noexcept
-  {
-    return a * scal;
-  }
-  [[nodiscard]] friend Vec2D abs(const Vec2D& a) noexcept
-  {
-    const auto bx = a.x < 0 ? -1 * a.x : a.x;
-    const auto by = a.y < 0 ? -1 * a.y : a.y;
-    return Vec2D(bx, by);
-  }
-  [[nodiscard]] friend float magnitude(const Vec2D& a) noexcept
-  {
-    return sqrt(a.x * a.x + a.y * a.y);
-  }
+  friend inline auto operator+(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> Vec2D<T> { return {a.x + b.x, a.y + b.y}; }
+  friend inline auto operator-(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> Vec2D<T> { return a + (-1)*b; }
+  friend inline auto operator*(const int scal, const Vec2D<T>& a) noexcept
+    -> Vec2D<T> { return mapIt([&scal](T a){return scal * a;}, a); }
+  friend inline auto operator*(const Vec2D<T>& a, const int scal) noexcept
+    -> Vec2D<T> { return scal * a; }
+  friend inline auto magnitude(const Vec2D<T>& a) noexcept
+    -> double {
+      return sqrt(foldIt(std::plus<double>{},
+        mapIt([](double a){return a * a;},
+            mapIt([](T a){return (double)a;}, a))));
+    }
+  friend inline auto normal(const Vec2D<T>& a) noexcept
+    -> Vec2D<double> {
+      return mapIt([mag = magnitude(a)](double a){return a / mag;},
+          mapIt([](T a){return (double)a;}, a));
+    }
 
-  [[nodiscard]] friend bool operator==(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return a.x == b.x && a.y == b.y;
-  }
-  [[nodiscard]] friend bool operator!=(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return !(a == b);
-  }
-  [[nodiscard]] friend bool operator<(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return magnitude(a) < magnitude(b);
-  }
-  [[nodiscard]] friend bool operator<=(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return !(a > b);
-  }
-  [[nodiscard]] friend bool operator>(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return magnitude(a) > magnitude(b);
-  }
-  [[nodiscard]] friend bool operator>=(const Vec2D& a, const Vec2D& b) noexcept
-  {
-    return !(a < b);
-  }
+  template<class Callable>
+  friend inline auto mapIt(Callable&& fn, const Vec2D<T>& a) noexcept
+    -> Vec2D<decltype(fn(a.x))> { return {fn(a.x), fn(a.y)}; }
+  template<class Callable>
+  friend inline auto foldIt(Callable&& fn, const Vec2D<T>& a) noexcept
+    -> decltype(fn(a.x, a.y)) { return fn(a.x, a.y); }
 
+  friend inline auto operator==(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> bool { return foldIt(std::logical_and{}, a <= b); }
+  friend inline auto operator!=(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> bool { return !(a == b); }
+  friend inline auto operator<(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> Vec2D<bool> { return {a.x < b.x, a.y < b.y}; }
+  friend inline auto operator>(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> Vec2D<bool> { return {a.x > b.x, a.y > b.y}; }
+  friend inline auto operator<=(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> Vec2D<bool> { return mapIt(std::logical_not{}, a > b); }
+  friend inline auto operator>=(const Vec2D<T>& a, const Vec2D<T>& b) noexcept
+    -> Vec2D<bool> { return mapIt(std::logical_not{}, a < b); }
 };
-
-#endif  // ! VEC2D_H
+#endif // ! VEC2D_H
