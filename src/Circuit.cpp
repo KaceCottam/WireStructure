59d3460a54922ef0baa09c399f43c45ddf54fd6b
Kace Cottam <kace.cottam@wsu.edu>
2020-02-12 11:46:18 -0800

    Changed static_cast to dynamic_cast

--- src/Circuit.cpp
+++ src/Circuit.cpp
@@ -2,10 +2,10 @@
 
 void Circuit::addNode(typename NodeSet::key_type key) {
   containedNodes.emplace(key);
-  if(auto in = static_cast<const Input*>(key)) {
+  if(auto in = dynamic_cast<const Input*>(key)) {
     if(inputs.count(in->inputDir) != 0) { throw in; }
     inputs[in->inputDir] = in;
-  } else if(auto out = static_cast<const Output*>(key)) {
+  } else if(auto out = dynamic_cast<const Output*>(key)) {
     if(outputs.count(out->outputDir) != 0) { throw out; }
     outputs[out->outputDir] = out;
   }
